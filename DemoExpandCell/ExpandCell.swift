64716bb8572dea4b90b684858b305d116408158a
Waranyu Khongchai <waranyu.khongchai@clbs.co.th>
2021-04-20 13:36:15 +0700

    EDIT code for expand cell

---
+++ DemoExpandCell/ExpandCell.swift
@@ -0,0 +1,23 @@
+//
+//  ExpandCell.swift
+//  DemoExpandCell
+//
+//  Created by PHENIXCRIME on 20/4/2564 BE.
+//
+
+import UIKit
+
+class ExpandCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- DemoExpandCell/ViewController.swift
+++ DemoExpandCell/ViewController.swift
@@ -7,13 +7,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class Section {
+    let title: String
+    let options: [String]
+    var isOpened = false
+    
+    init(title: String,
+         options: [String],
+    isOpened: Bool = false) {
+    self.title = title
+    self.options = options
+    self.isOpened = isOpened
     }
-
-
 }
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var sections = [Section]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set up Models
+        sections = [
+            Section(title: "Section 1", options: [1, 2, 3].compactMap({return "Cell \($0)"})),
+            Section(title: "Section 2", options: [1, 2, 3].compactMap({return "Cell \($0)"})),
+            Section(title: "Section 3", options: [1, 2, 3].compactMap({return "Cell \($0)"})),
+            Section(title: "Section 4", options: [1, 2, 3].compactMap({return "Cell \($0)"})),
+            Section(title: "Section 5", options: [1, 2, 3].compactMap({return "Cell \($0)"}))
+        ]
+        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.frame = view.bounds
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        
+        if section.isOpened {
+            return section.options.count + 1
+        }
+        else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        if indexPath.row == 0 {
+            cell.textLabel?.text = sections[indexPath.section].title
+        }
+        else  {
+            cell.textLabel?.text = sections[indexPath.section].options[indexPath.row - 1]
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+            tableView.reloadSections([indexPath.section], with: .none)
+        }
+        else {
+           print("Tapped sub cell")
+        }
+    }
+}
